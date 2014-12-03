46de3f0ddba5a7611f025b816d637136593b20b1
Artur Malabarba <bruce.connor.am@gmail.com>
2014-12-03 14:13:29 +0000

    Not adamant

--- sx-compose.el
+++ sx-compose.el
@@ -20,9 +20,9 @@
 ;;; Commentary:
 
 ;; This file defines `sx-compose-mode' and its auxiliary functions and
-;; variables. In order to use `sx-compose-mode', it is adamant that
-;; the variable `sx-compose--send-function' be set. Otherwise it's
-;; just a regular markdown buffer.
+;; variables. In order to use `sx-compose-mode', it is vital that the
+;; variable `sx-compose--send-function' be set. Otherwise it's just a
+;; regular markdown buffer.
 ;;
 ;; In order to help avoid mistakes, there is the function
 ;; `sx-compose-create'. This is the preferred way of activating the
