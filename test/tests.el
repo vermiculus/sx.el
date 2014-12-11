7dc4e4f83599ba9ee3ca1af9efd8e8ce9112316c
Artur Malabarba <bruce.connor.am@gmail.com>
2014-12-11 11:36:02 +0000

    Fix unrelated test

--- test/tests.el
+++ test/tests.el
@@ -106,17 +106,17 @@
     (goto-char (point-min))
     (should (equal (buffer-name) "*question-list*"))
     (line-should-match
-     "^\\s-+1\\s-+0\\s-+Focus-hook: attenuate colours when losing focus [ 0-9]+[ydhms] ago\\s-+\\[frames\\] \\[hooks\\] \\[focus\\]")
+     "^\\s-+1\\s-+0\\s-+Focus-hook: attenuate colours when losing focus [ 0-9]+[ydhms]o? ago\\s-+\\[frames\\] \\[hooks\\] \\[focus\\]")
     (sx-question-list-next 5)
     (line-should-match
-     "^\\s-+0\\s-+1\\s-+Babel doesn&#39;t wrap results in verbatim [ 0-9]+[ydhms] ago\\s-+\\[org-mode\\]")
+     "^\\s-+0\\s-+1\\s-+Babel doesn&#39;t wrap results in verbatim [ 0-9]+[ydhms]o? ago\\s-+\\[org-mode\\]")
     ;; ;; Use this when we have a real sx-question buffer.
     ;; (call-interactively 'sx-question-list-display-question)
     ;; (should (equal (buffer-name) "*sx-question*"))
     (switch-to-buffer "*question-list*")
     (sx-question-list-previous 4)
     (line-should-match
-     "^\\s-+2\\s-+1\\s-+&quot;Making tag completion table&quot; Freezes/Blocks -- how to disable [ 0-9]+[ydhms] ago\\s-+\\[autocomplete\\]")))
+     "^\\s-+2\\s-+1\\s-+&quot;Making tag completion table&quot; Freezes/Blocks -- how to disable [ 0-9]+[ydhms]o? ago\\s-+\\[autocomplete\\]")))
 
 (ert-deftest macro-test--sx-assoc-let ()
   "Tests macro expansion for `sx-assoc-let'"
