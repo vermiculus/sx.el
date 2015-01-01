9469287080501f3e3c7ce0002d837664a1b9b91e
Sean Allred <code@seanallred.com>
2015-01-01 13:15:20 -0500

    Use test fixture for sample data
    See (info "(ert) Fixtures and Test Suites").

--- test/test-macros.el
+++ test/test-macros.el
@@ -1,18 +1,21 @@
+(defmacro sx-test-with-json-data (cell &rest body)
+    (declare (indent 1))
+    `(let ((,cell '((test . nil) (test-one . 1) (test-two . 2)
+                   (link . "http://meta.emacs.stackexchange.com/"))))
+       ,@body))
+
 (ert-deftest macro-test--sx-assoc-let ()
   "Tests macro expansion for `sx-assoc-let'"
-  (let ((prototype '((test . nil) (test-one . 1) (test-two . 2)
-                     (link . "http://meta.emacs.stackexchange.com/"))))
-    (let ((data (copy-tree prototype)))
-      (should
-       (null (let-alist data .site))))
+  (sx-test-with-json-data data
+    (should
+     (null (let-alist data .site))))
 
-    (let ((data (copy-tree prototype)))
-      (should
-       (equal (sx-assoc-let data .site)
-              "meta.emacs")))
+  (sx-test-with-json-data data
+    (should
+     (equal (sx-assoc-let data .site)
+            "meta.emacs")))
 
-    (let ((data (copy-tree prototype)))
-      (should
-       (equal (sx-assoc-let data (cons .test-one .test-two))
-              '(1 . 2))))))
-
+  (sx-test-with-json-data data
+    (should
+     (equal (sx-assoc-let data (cons .test-one .test-two))
+            '(1 . 2)))))
