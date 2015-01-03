52a8682c762bdc5e2e8d3a9ccc2eb527556f58ad
Sean Allred <code@seanallred.com>
2015-01-02 22:15:44 -0500

    Message actions

--- bot/sx-bot.el
+++ bot/sx-bot.el
@@ -37,9 +37,11 @@
 (defun sx-bot-write-to-file (data)
   "Write (cdr DATA) to file named (car DATA).
 File is savedd in `sx-bot-out-dir'."
-  (with-temp-file (expand-file-name (car data) sx-bot-out-dir)
-    (let (print-length)
-      (prin1 (cdr data) (current-buffer)))))
+  (let ((file-name (expand-file-name (car data) sx-bot-out-dir)))
+    (message "Writing %S" file-name)
+    (with-temp-file file-name
+      (let (print-length)
+        (prin1 (cdr data) (current-buffer))))))
 
 
 (defun sx-bot-fetch-and-write-tags ()
