4722414ceb966154b0b32ff32ee8b652eae4a285
Sean Allred <code@seanallred.com>
2015-01-04 15:11:04 -0500

    Statistical analysis functions

---
+++ data/stats.el
@@ -0,0 +1,46 @@
+;;; stats.el --- data statistics                     -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  Sean Allred
+
+;; Author: Sean Allred <code@seanallred.com>
+;; Keywords: data
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Provides statistical analysis functions for cached data.
+
+;;; Code:
+
+(defun sx--stat-avg (numbers)
+  (/ (float (apply #'+ numbers)) (length numbers)))
+
+(defun sx--stat-avg-tag-length ()
+  "The average length of a tag across all sites.
+Makes no account for tag popularity."
+  (sx--stat-avg
+   (mapcar (lambda (file)
+             (sx--stat-avg
+              (mapcar #'length
+                      (read
+                       (with-temp-buffer
+                         (insert-file-contents
+                          (format "tags/%s" file))
+                         (buffer-string))))))
+           (cddr (sort (directory-files "tags")
+                       #'string-lessp)))))
+
+(provide 'sx-stats)
+;;; stats.el ends here
