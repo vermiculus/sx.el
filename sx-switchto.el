25cf5bf2e784ac870fa4c6f00502eda6dc8e7f22
Artur Malabarba <bruce.connor.am@gmail.com>
2015-01-04 12:06:23 -0200

    Define some keys conditionally

--- sx-switchto.el
+++ sx-switchto.el
@@ -43,12 +43,30 @@
     ("f" sx-tab-featured)
     ("i" sx-inbox)
     ("n" sx-tab-newest)
-    ("s" sx-question-list-switch-site)
     ("t" sx-tab-switch)
     ("U" sx-tab-unanswered-my-tags)
     ("v" sx-tab-topvoted)
     ("w" sx-tab-week)
     ))
+
+
+;;; These are keys which depend on context.
+;;;; For instance, it makes no sense to have `switch-site' bound to a
+;;;; key on a buffer with no `sx-question-list--site' variable.
+(defmacro sx--define-conditional-key (keymap key def &rest body)
+  "In KEYMAP, define key sequence KEY as DEF conditionally.
+This is like `define-key', except the definition \"disapears\"
+whenever BODY evaluates to nil."
+  (declare (indent 3)
+           (debug (form form form &rest sexp)))
+  `(define-key ,keymap ,key
+     '(menu-item
+       ,(format "maybe-%s" (or (car (cdr-safe def)) def)) ignore
+       :filter (lambda (&optional _)
+                 (when (progn ,@body) ,def)))))
+
+(sx--define-conditional-key sx-switchto-map "s" #'sx-question-list-switch-site
+  (and (boundp 'sx-question-list--site) sx-question-list--site))
 
 (provide 'sx-switchto)
 ;;; sx-switchto.el ends here
