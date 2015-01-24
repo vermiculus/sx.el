76bfce36a4808e71aec27fdc2f7f8cef8462500f
Artur Malabarba <bruce.connor.am@gmail.com>
2015-01-23 23:41:13 -0200

    Add tests for sx-request-get-...

--- test/test-api.el
+++ test/test-api.el
@@ -14,3 +14,11 @@
 (ert-deftest test-method-get-all ()
   "Tests sx-method interface to `sx-request-all-items'"
   (should (< 250 (length (sx-method-call 'sites :get-all t)))))
+
+(ert-deftest request-get ()
+  (should (sx-request-get-url "http://google.com"))
+  (should-error (sx-request-get-url "http://github.com/Bruce-Connor/does-not-exist"))
+  (should-error (sx-request-get-data "tags/emacs-does-not-exist"))
+  (let ((emacs-tags (length (sx-request-get-data 'tags/emacs))))
+    (should (> emacs-tags 450))
+    (should (not (cl-remove-if #'stringp emacs-tags)))))
